3a6e8497a0c975c56839c71ffbc02a8392140a34
陈俊州 <chenjzh@qdum.com>
2017-12-01 11:09:19 +0800

    增加文件夹遍历

--- utils/files.go
+++ utils/files.go
@@ -1,6 +1,11 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"fmt"
+	"strings"
+	"io/ioutil"
+)
 
 /**
  * 判断指定文件是否存在
@@ -10,4 +15,32 @@ func FileExists(path string) (bool, error) {
 	if err == nil { return true, nil }
 	if os.IsNotExist(err) { return false, nil }
 	return true, err
+}
+
+/**
+	获取指定目录中的文件列表
+	:param file_path: 指定的路径
+	:return: []string, error
+	:notice
+		只遍历文件忽略文件夹
+ */
+func FileListInPath(filePath string) ([]string, error) {
+	var resultList []string
+	dirList, err := ioutil.ReadDir(filePath)
+	if err != nil {
+		fmt.Println("read dir error")
+		return nil, err
+	}
+
+	for _, v := range dirList {
+		fileName := v.Name()
+		fileMode := v.Mode()
+		if 0 == strings.Index(fileName, ".") || fileMode.IsDir() {
+			continue
+		}
+
+		resultList = append(resultList, fileName)
+	}
+
+	return resultList, nil
 }
